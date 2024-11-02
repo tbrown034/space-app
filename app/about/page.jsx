7d3b806d49a4a1de827d5f38bff974411b8e8d57
tbrown034 <tbrown034@gmail.com>
2024-11-02 18:58:09 -0400

    better routing

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,10 +3,13 @@ import Link from "next/link";
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4 p-4 ">
-      <h1>About</h1>
-      <div>
+      <h1 className="text-2xl">About</h1>
+      <div className="flex gap-4">
         <Link className="p-2 border-2 border-white rounded-xl" href="/">
           Home
+        </Link>
+        <Link className="p-2 border-2 border-white rounded-xl" href="/play">
+          Play
         </Link>
       </div>
     </div>
--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -1,13 +1,19 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-4 p-4 ">
-      <h1>Admin</h1>
-      <div>
-        <Link className="p-2 border-2 border-white rounded-xl" href="/admin">
+      <h1 className="text-2xl">Admin</h1>
+      <div className="flex gap-4">
+        <Link className="p-2 border-2 border-white rounded-xl" href="/">
           Home
         </Link>
+        <Link className="p-2 border-2 border-white rounded-xl" href="/play">
+          Play
+        </Link>
+        {/* <Link>Get Random Pair</Link> */}
+        {/* so this should 1. generate a random date between 1/1/2022 and whatever is the current date.  2.. Check our database, if there is a database, and see whether there is an entry for that date already. If there is, tell admin alread do a fetch to nasa's api, get the nasa photo of the day and it's metadata, then it should */}
       </div>
     </div>
   );
